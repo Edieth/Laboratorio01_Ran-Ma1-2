18b18abde8a8b8a692b93d6d960b958f984a8c54
Edieth <edloriama@est.utn.ac.cr>
2024-09-28 23:16:06 -0600

    Actualizacion3

--- Recetas.py
+++ Recetas.py
@@ -1,26 +1,28 @@
-from msilib.schema import Class
-
 
 class Recetas:
     def __init__(self, v1=0, v2="", v3=""):
         self.__Cantidad = v1
-        self.__Unidad = v2
+        self.__Unidades = v2
         self.__Ingrediente = v3
 
-
+    def setCantidad(self, cant):
+        self.__Cantidad = cant
 
     def getCantidad(self):
         return self.__Cantidad
 
+    def setUnidad(self, unidad):
+        self.__Unidades = unidad
+
     def getUnidad(self):
-        return self.__Unidad
+        return self.__Unidades
+
+    def setCantidad(self, Ing):
+        self.__Ingrediente = Ing
 
     def getIngrediente(self):
         return self.__Ingrediente
 
 
-class Preparaciones(Recetas):
-    __identi = 0
-    __ModoPrepara = ''
 
-    def __init__(self,):
+
